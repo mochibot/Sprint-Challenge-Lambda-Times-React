53374c7567e12ce7b5d2c266f2afd29c856cff6a
mochibot <amusaur@gmail.com>
2019-06-07 09:16:34 -0700

    Refactored topbar and header to styled components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const HeaderDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    .header {
+      width: 1280px;
+    }
+  }
+
+  > h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+`
+
+const HeaderSpan = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`
+
+const HeaderDateSpan = styled(HeaderSpan)`
+  margin-left: 25px;
+  flex: 1;
+`
+const HeaderTempSpan = styled(HeaderSpan)`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`
+//End of styled components
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <HeaderDiv>
+      <HeaderDateSpan>SMARCH 32, 2018</HeaderDateSpan>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <HeaderTempSpan>98°</HeaderTempSpan>
+    </HeaderDiv>
   )
 }
 
